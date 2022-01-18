121e5f9dcc146332c0360d2da0b481e44bcd02ca
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-18 17:49:53 -0500

    Se crean los useCaseHandle

---
+++ src/main/java/co/com/sofka/infra/handle/AsignarPeliculaUseCaseHandle.java
@@ -0,0 +1,27 @@
+package co.com.sofka.infra.handle;
+
+import co.com.sofka.domain.catalogo.command.AsignarPeliculaCommand;
+import co.com.sofka.infra.generic.UseCaseHandle;
+import co.com.sofka.useCases.ExtraerCatalogoUseCase;
+import co.com.sofka.wsscore.domain.program.command.AssignScoreCommand;
+import co.com.sofka.wsscore.usecases.ExtractScoreUseCase;
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class AsignarPeliculaUseCaseHandle extends UseCaseHandle {
+
+    private final ExtraerCatalogoUseCase extraerCatalogoUseCase;
+
+    public AsignarPeliculaUseCaseHandle(ExtraerCatalogoUseCase extraerCatalogoUseCase) {
+        this.extraerCatalogoUseCase = extraerCatalogoUseCase;
+    }
+
+    @ConsumeEvent(value = "sofkau.program.assignscore")
+    void consumeBlocking(AsignarPeliculaCommand command) {
+        var events = extraerCatalogoUseCase.apply(command);
+        saveCatalogo(command.getCatalogoId(), events);
+    }
+}
