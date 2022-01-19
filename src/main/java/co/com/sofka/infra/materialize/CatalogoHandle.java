bdb237744198499040acf31abff15d6adce23af1
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2022-01-19 16:25:23 -0500

    Se añaden cambios para que catalogo traiga todas las películas

--- src/main/java/co/com/sofka/infra/materialize/CatalogoHandle.java
+++ src/main/java/co/com/sofka/infra/materialize/CatalogoHandle.java
@@ -36,12 +36,11 @@ public class CatalogoHandle {
     @ConsumeEvent(value = "sofka.peliculaasignada", blocking = true)
     void consumePeliculaAsignada(PeliculaAsignada event) {
         BasicDBObject document = new BasicDBObject();
-        var key = "catalogo."+event.getAggregateId();
-        document.put(key+".pelicula"+".nombre", event.getNombre());
-        document.put(key+".pelicula"+".fecha", event.getFecha());
-        document.put(key+".pelicula"+".sinopsis", event.getSinopsis());
-        document.put(key+".pelicula"+".genero", event.getGenero());
-        document.put(key+".pelicula"+".url", event.getUrl());
+        document.put("pelicula."+event.getNombre()+".nombre", event.getNombre());
+        document.put("pelicula."+event.getNombre()+".fecha", event.getFecha());
+        document.put("pelicula."+event.getNombre()+".sinopsis", event.getSinopsis());
+        document.put("pelicula."+event.getNombre()+".genero", event.getGenero());
+        document.put("pelicula."+event.getNombre()+".url", event.getUrl());
 
         BasicDBObject updateObject = new BasicDBObject();
         updateObject.put("$set", document);
